bd0a8e77d2df4dd2f571eb79a79a09fe6cc69cdf
Victor Hugo <102314380+vitohg@users.noreply.github.com>
2023-05-04 13:56:42 -0300

    Add files via upload

--- cpf-certifier/script.js
+++ cpf-certifier/script.js
@@ -5,9 +5,64 @@ const input = document.querySelector("input")
 input.addEventListener("keypress", () => {
   let inputValue = input.value.length
 
-  console.log(inputValue);
+  if (inputValue === 3 || inputValue === 7) {
+    input.value += "."
+  }else if (inputValue === 11) {
+    input.value += "-"
+  }
     
 });
+
+function cpfValid(cpf = 0) {
+    cpf = cpf.value;
+    
+    let soma = 0;
+
+    soma += cpf[0] * 10;
+    soma += cpf[1] * 9;
+    soma += cpf[2] * 8;
+    soma += cpf[4] * 7;
+    soma += cpf[5] * 6;
+    soma += cpf[6] * 5;
+    soma += cpf[8] * 4;
+    soma += cpf[9] * 3;
+    soma += cpf[10] * 2;
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    if (soma != cpf[9]){
+
+    }
+
+    soma = 0;
+
+    soma += cpf[0] * 11;
+    soma += cpf[1] * 10;
+    soma += cpf[2] * 9;
+
+    soma += cpf[4] * 8;
+    soma += cpf[5] * 7;
+    soma += cpf[6] * 6;
+
+    soma += cpf[8] * 5;
+    soma += cpf[9] * 4;
+    soma += cpf[10] * 3;
+
+    soma += cpf[12] * 2;
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    if (soma != cpf[12]){
+
+    }
+    
+}
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -17,13 +72,16 @@ form.addEventListener("submit", (e) => {
 
 function checkInputs() {
     const cpfValue = cpf.value;
+    let ncaracteries = input.value.length
 
     if (cpfValue === "") {
         setErrorFor(cpf, "O CPF é obrigatório.");
 
-    }else{
-        setSuccessFor(cpf, "O CPF é valido.");
+    }else if (ncaracteries < 14){
+        setErrorFor(cpf, "O CPF está incompleto");
 
+    }else {
+        setSuccessFor(cpf, "O CPF é valido.");
 
     }
 }
@@ -45,4 +103,3 @@ function setSuccessFor(input, message) {
 
     formControl.className = "form-control success";
 }
-

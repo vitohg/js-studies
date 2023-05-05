700e8a7b05c3b9925539443dc4fb3b3dec1bf154
Victor Hugo <102314380+vitohg@users.noreply.github.com>
2023-05-05 13:48:29 -0300

    Add files via upload

--- cpf-certifier/script.js
+++ cpf-certifier/script.js
@@ -3,20 +3,29 @@ const cpf = document.getElementById("cpf");
 const input = document.querySelector("input")
 
 input.addEventListener("keypress", () => {
-  let inputValue = input.value.length
+    let inputValue = input.value.length
 
-  if (inputValue === 3 || inputValue === 7) {
-    input.value += "."
-  }else if (inputValue === 11) {
-    input.value += "-"
-  }
-    
+    if (inputValue === 3 || inputValue === 7) {
+        input.value += "."
+    } else if (inputValue === 11) {
+        input.value += "-"
+    }
+
 });
 
 //function cpfValid(cpf = 0) {  
 //}
 
-
+function arraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;
+    
+    for (var i = 0; i < a.length; ++i) {
+      if (a[i] !== b[i]) return false;
+    }
+    return true;
+  }
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -29,80 +38,105 @@ function checkInputs() {
     let ncaracteries = input.value.length
     let soma = 0;
 
+
+
+
     if (cpfValue === "") {
-        setErrorFor(cpf, "O CPF é obrigatório.");
+        setErrorFor(cpfValue, "O CPF é obrigatório.");
+
+    } else if (ncaracteries < 14) {
+        setErrorFor(cpfValue, "O CPF está incompleto");
+
+    }
+
+    // Calculo do cpf
+
+    else if (ncaracteries == 14) {
+        let resultado = []
+        
+
+        for (let i = 0; i < cpfValue.length; i++) {
+            if (isNaN(cpfValue[i])) {
+
+            } else {
+                resultado.push(Number(cpfValue[i]))
+            }
+
+        }
+
+
+
+        let ok = 0
+        let teste_matriz = []
+
+        for (let i = 0; i < 10; i++) {
+            teste_matriz.push([i, i, i, i, i, i, i, i, i, i, i])
+        }
+
+        for (let i = 0; i < teste_matriz.length; i++) {
+            if (arraysEqual(teste_matriz[i], resultado)) {
+                ok = 1
+                break;
+            }            
+        }
+        if (ok === 0) {
+
+            for (let i = 0; i < (resultado.length - 2); i++) {
+                soma += resultado[i] * (10 - i)            
+            }
     
-    }else if (ncaracteries < 14){
-        setErrorFor(cpf, "O CPF está incompleto");
-
-    }
-
-        // Calculo do cpf
-
-    else if (cpfValue) {
-            
-        soma += cpf[0] * 10;
-        soma += cpf[1] * 9;
-        soma += cpf[2] * 8;
-        soma += cpf[4] * 7;
-        soma += cpf[5] * 6;
-        soma += cpf[6] * 5;
-        soma += cpf[8] * 4;
-        soma += cpf[9] * 3;
-        soma += cpf[10] * 2;
-        resultSoma1 = (soma * 10) % 11;
-
-        if (resultSoma1 == 10 || resultSoma1 == 11) {
-            resultSoma1 = 0;
-
-            setSuccessFor(cpf, "O CPF é valido");
-
-        }else if (resultSoma1 != cpf[12]){
-            setErrorFor(cpf, "O CPF é invalido");
-
-        }else{
-            setSuccessFor(cpf, "O CPF é valido");
-
-        }
-
-        soma = 0;
-
-        soma += cpf[0] * 11;
-        soma += cpf[1] * 10;
-        soma += cpf[2] * 9;
-        soma += cpf[4] * 8;
-        soma += cpf[5] * 7;
-        soma += cpf[6] * 6;
-        soma += cpf[8] * 5;
-        soma += cpf[9] * 4;
-        soma += cpf[10] * 3;
-        soma += cpf[12] * 2;
-        resultSoma2 = (soma * 10) % 11;
-
-        if (resultSoma2 == 10 || resultSoma2 == 11) {
-            resultSoma2 = 0;
-
-            setSuccessFor(cpf, "O CPF é valido");
-
-        }else if (resultSoma2 != cpf[13]){
-            setErrorFor(cpf, "O CPF é invalido");
-
-        }else{
-            setSuccessFor(cpf, "O CPF é valido");
-
+            resultSoma1 = soma % 11;
+    
+    
+            if (resultSoma1 < 2) {
+                resultSoma1 = 0
+            } else {
+                resultSoma1 = 11 - resultSoma1
+            }
+    
+            // [1, 1, 1, 4, 4, 4, 7, 7, 7, 3, 5]
+    
+            soma = 0;
+            for (let i = 0; i < (resultado.length - 2); i++) {
+                soma += resultado[i] * (11 - i)   
+            }
+    
+            soma += resultSoma1 * 2;
+            resultSoma2 = soma % 11;
+    
+            if (resultSoma2 < 2) {
+                resultSoma2 = 0
+            } else {
+                resultSoma2 = 11 - resultSoma2
+            }
+    
+            console.log("Digito 1:"+resultSoma1)
+            console.log("Digito 2:"+resultSoma2)
+    
+            if(resultado[9] != resultSoma1) {
+                console.log("CPF inválido")
+            } else {
+                if(resultado[10] != resultSoma2) {
+                    console.log("CPF inválido")
+                } else {
+                    console.log("CPF válido")
+                }
+            }
+        } else {
+            console.log("CPF inválido")
         }
 
     }
 
-        // Fim do calculo do cpf
+    // Fim do calculo do cpf
 
 }
-    //else if(cpfValid){
-    //    setSuccessFor(cpf, "O CPF é valido.");
+//else if(cpfValid){
+//    setSuccessFor(cpf, "O CPF é valido.");
 
-    //}else{
-    //   setErrorFor(cpf, "O CPF é invalido")
-    //}
+//}else{
+//   setErrorFor(cpf, "O CPF é invalido")
+//}
 
 
 function setErrorFor(input, message) {

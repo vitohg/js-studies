b34351c141efce40d1ab26a3cb57c238e3259a96
Victor Hugo <102314380+vitohg@users.noreply.github.com>
2023-05-12 11:13:26 -0300

    Add files via upload

--- age-calculator/script.js
+++ age-calculator/script.js
@@ -1,47 +1,25 @@
-function calcularIdade() {
-
-let dia1 = document.getElementById('dia1').value
-let mes1 = document.getElementById('mes1').value
-let ano1 = document.getElementById('ano1').value
-
-let data = new Date()
-
-let dia2 = data.getDate()
-let mes2 = data.getMonth()
-let ano2 = data.getFullYear()
-
-let ultimoDiadeCadaMes = [31, 28, 31, 30, 31, 30 ,31, 31, 30, 31, 30, 31]
-
-if (dia2 > dia1) {
-
-    dia2 = dia2 + ultimoDiadeCadaMes [mes2 - 1]
-    mes2 = mes2 + 1
-    
-}
-
-if (mes1 > mes2) {
-    
-    mes2 = mes2 + 12
-    ano2 = ano2 - 1
-
-}
-
-let dia = dia2 - dia1
-let mes = mes2 - mes2
-let ano = ano2 - ano1
-
-document.getElementById("idade").innerHTML = "você tem ${ano} anos, ${mes} meses e ${dia} dias."
-
-    
-}
-
 const form = document.getElementById("form");
-const day = document.getElementById("day").value
-const month = document.getElementById("month").value
-const year = document.getElementById("year").value
-let input = document.querySelector("input");
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
 
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
 
+    VerificarInput();
+})
+
+function VerificarInput() {
+
+    const dayValue = day.value;
+    const monthValue = month.value;
+    const yearValue = year.value;
+
+    if (dayValue === "") {
+        setErrorFor(day, "Digite o dia...");
+    }
+
+}
 function calculateAge() {
 
     let data = new Date();
@@ -76,19 +54,19 @@ function calculateAge() {
 
 function setErrorFor(input, mensage) {
     const formControl = input.parentElement;
-    const small = formControl.querySelector("small");
+    const small = formControl.querySelector("small")
 
     small.innerText = mensage;
 
-    formControl.className = "form-comtrol error";
+    formControl.className = "form-control error";
 
 }
 
 function setSuccesFor(input, mensage) {
     const formControl = input.parentElement;
-    const small = formControl.querySelector("small");
+    const small = formControl.querySelector("small")
 
     small.innerText = mensage;
 
-    formControl.className = "form-coontrol succes";
+    formControl.className = "form-control succes";
 }
